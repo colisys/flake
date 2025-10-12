74724c49c10650578674fbe40f381b8ea47a6908
SpikeMeow <colisys@duck.com>
2025-10-12 18:24:39 +0800

    Add examples and comments

--- public/index.php
+++ public/index.php
@@ -8,18 +8,6 @@ use Flake\Request;
 use Flake\Response;
 use Flake\Router;
 
-Router::post('/data/:id/meow', function (Request $req, Response $res, $id) {
-    // print_r($req->getParams());
-    // $id   = $req->get('id', 'unknown');
-    $body = $req->body();
-    $res->json([
-        'id'            => $req->get('id', 'unknown'),
-        'received_id'   => $id,
-        'received_body' => json_decode($body, true),
-        'message'       => $req->post('data.message', 'Hello World!'),
-    ]);
-});
-
 // Home route
 Router::get('/', function ($req, $res) {
     $output = "
@@ -46,6 +34,53 @@ Router::get('/json', function (Request $req, Response $res) {
         'message'   => 'Welcome to Flake!',
         'version'   => '1.0.0',
         'timestamp' => time(),
+    ]);
+});
+
+// Get route with parameter
+Router::get('/user/:id', function (Request $req, Response $res, $id) {
+    // All route parameters will injected as function arguments
+    $res->json([
+        // Get query parameter
+        'id'  => $req->get('id', 'unknown'),
+        // Get route parameter
+        'received_id' => $id,
+    ]);
+});
+
+// Route with parameter
+Router::post('/user/:id', function (Request $req, Response $res, $id) {
+    // All route parameters will injected as function arguments
+    $body = $req->body();
+    $res->json([
+        // Get query parameter
+        'id'            => $req->get('id', 'unknown'),
+        // Get route parameter
+        'received_id'   => $id,
+        // Get raw body and parse it as JSON
+        'received_body' => json_decode($body, true),
+        // Get nested data using dot notation
+        'message'       => $req->post('data.message', 'Hello World!'),
+        // Also works with index
+        'list'          => $req->post('data.list.2'),
+    ]);
+});
+
+// PUT route example
+Router::put('/user/:id', function (Request $req, Response $res, $id) {
+    $data = $req->json();
+    $res->json([
+        'method' => 'PUT',
+        'id'     => $id,
+        'data'   => $data,
+    ]);
+});
+
+// DELETE route example
+Router::delete('/user/:id', function (Request $req, Response $res, $id) {
+    $res->json([
+        'method' => 'DELETE',
+        'id'     => $id,
     ]);
 });
 
