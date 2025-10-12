49eed7c38f826843c33c7b70e36bd40d81fea159
SpikeMeow <colisys@duck.com>
2025-10-12 22:48:56 +0800

    Add event listeners for app start and route not found

--- public/index.php
+++ public/index.php
@@ -3,9 +3,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Flake\App;
+use Flake\Events;
 
 require __DIR__ . '/../routes/examples.php';
 require __DIR__ . '/../middlewares/examples.php';
 
+Events::listen('App.Start', static fn() => error_log('App started'));
+Events::listen('Router.NotFound', static fn(Throwable $th) => error_log('Route not found: ' . $th->getMessage()));
 
 App::run();
