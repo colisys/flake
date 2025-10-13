944841237acc7644d44841c3e7759f0e0cbff2d1
Ian <ian.escarro@gmail.com>
2025-10-13 12:09:56 +0800

    Remove reference to users controller

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Flake\App;
 use Flake\Router;
 
-require_once __DIR__ . '/../routes/users.php';
-
 Router::get('/', function ($req, $res) {
     $res->send('Hello, world!');
 });
