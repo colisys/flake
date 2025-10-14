007dd42cc703b523c3e9ffe903ef355b06ef9048
SpikeMeow <colisys@duck.com>
2025-10-14 17:05:39 +0800

    Enhance Router with class-string callbacks and error handling

--- src/Router.php
+++ src/Router.php
@@ -1,18 +1,22 @@
 <?php
-
 namespace Flake;
 
 class Router
 {
     /**
-     * @var array<string, array<string, \Closure(Request $request, Response $response)|\Closure(Request $request, Response $response, Middleware $next)>>
+     * @var array<string, array<string, \Closure(Request $request, Response $response)|array{0: class-string, 1: string}>>
      */
     private static array $routes = [];
 
     /**
-     * @var \Closure(Request $request, Response $response)
+     * @var ?\Closure(Request $request, Response $response)
      */
     private static $fallback = null;
+
+    /**
+     * @var ?\Closure(Request $request, Response $response)
+     */
+    private static $error = null;
 
     public function __construct()
     {
@@ -29,7 +33,7 @@ class Router
      * Add a GET route
      *
      * @param string $uri
-     * @param \Closure(Request $request, Response $response, ...$params) $callback
+     * @param \Closure(Request $request, Response $response, ...$params)|array{0: class-string, 1: string} $callback
      */
     public static function get($uri, $callback)
     {
@@ -40,7 +44,7 @@ class Router
      * Add a POST route
      *
      * @param string $uri
-     * @param \Closure(Request $request, Response $response, ...$params) $callback
+     * @param \Closure(Request $request, Response $response, ...$params)|array{0: class-string, 1: string} $callback
      */
     public static function post($uri, $callback)
     {
@@ -51,7 +55,7 @@ class Router
      * Add a PUT route
      *
      * @param string $uri
-     * @param \Closure(Request $request, Response $response, ...$params) $callback
+     * @param \Closure(Request $request, Response $response, ...$params)|array{0: class-string, 1: string} $callback
      */
     public static function put($uri, $callback)
     {
@@ -62,7 +66,7 @@ class Router
      * Add a PATCH route
      *
      * @param string $uri
-     * @param \Closure(Request $request, Response $response, ...$params) $callback
+     * @param \Closure(Request $request, Response $response, ...$params)|array{0: class-string, 1: string} $callback
      */
     public static function patch($uri, $callback)
     {
@@ -73,7 +77,7 @@ class Router
      * Add a DELETE route
      *
      * @param string $uri
-     * @param \Closure(Request $request, Response $response, ...$params) $callback
+     * @param \Closure(Request $request, Response $response, ...$params)|array{0: class-string, 1: string} $callback
      */
     public static function delete($uri, $callback)
     {
@@ -83,23 +87,27 @@ class Router
     /**
      * Add a route for any method
      *
-     * @param string $method
+     * @param string|array{"GET": 0, "POST": 1, "PUT": 2, "PATCH": 3, "DELETE": 4} $method
      * @param string $uri
-     * @param \Closure(Request $request, Response $response, ...$params) $callback
+     * @param \Closure(Request $request, Response $response, ...$params)|array{0: class-string, 1: string} $callback
      */
     public static function any($method, $uri, $callback)
     {
-        // Support controller class callbacks like [ProductController::class, 'destroy']
-        if (is_array($callback) && count($callback) === 2 && class_exists($callback[0])) {
-            $instance = new $callback[0]();
-            $callback = [$instance, $callback[1]];
-        }
+        match (true) {
+            is_callable($callback)                    => true,
+            (is_array($callback) && count($callback) === 2) &&
+            method_exists($callback[0], $callback[1]) => true,
+            default                                   => throw new \InvalidArgumentException('Callback must be callable')
+        };
 
-        if (!is_callable($callback)) {
-            throw new \InvalidArgumentException('Callback must be callable');
+        // TODO: Should we throw an error if the router already registed?
+        if (is_array($method)) {
+            foreach ($method as $m) {
+                self::$routes[$m][$uri] = $callback;
+            }
+        } else {
+            self::$routes[$method][$uri] = $callback;
         }
-
-        self::$routes[$method][$uri] = $callback;
     }
 
     /**
@@ -112,6 +120,12 @@ class Router
         self::$fallback = $callback;
     }
 
+    /**
+     * Dispatch the request to the appropriate route
+     *
+     * @param Request $request
+     * @param Response $response
+     */
     public static function dispatch(Request $request, Response $response): void
     {
         $uri    = $request->uri();
@@ -123,30 +137,128 @@ class Router
             $res->status(404)->send("Cannot " . $req->method() . " " . $req->uri());
         };
 
-        // Match route
-        if (isset($routes[$method])) {
-            // Exact match
-            if (isset($routes[$method][$uri]) && is_callable($routes[$method][$uri])) {
-                $callback = $routes[$method][$uri];
+        try {
+            // Ensure method exists
+            if (! isset($routes[$method])) {
+                throw new \InvalidArgumentException("Method {$method} not supported");
+            }
+
+            // Check if simple route exists
+            if (array_key_exists($uri, $routes[$method])) {
+                if (
+                    isset($routes[$method]) &&
+                    is_callable($routes[$method][$uri]) ||
+                    count($routes[$method][$uri]) === 2
+                ) {
+                    $callback = $routes[$method][$uri];
+                }
             } else {
                 // Check for parameterized routes
                 foreach ($routes[$method] as $route => $cb) {
-                    preg_match_all('#:([\w]+)#', $route, $paramNames);
-                    $routeRegex = preg_replace('#:([\w]+)#', '([\w-]+)', $route);
+                    // separate parameter name and value using regex
+                    preg_match_all('#:([\w]+)?#', $route, $paramNames);
+                    // Convert :param to regex
+                    $pattern = preg_replace('#:([\w]+)#', '([\w-]+)', $route);
+                    // Check for optional parameters, only last one is allowed
+                    $pattern2 = preg_replace('#\?\(\[\\\w\-\]\+\)$#', '?', $pattern);
+                    // Check if route matches
+                    if (preg_match("#^{$pattern}$#", $uri, $matches) || preg_match("#^{$pattern2}$#", $uri, $matches2)) {
+                        $matches = array_merge($matches ?? [], $matches2);
+                        if (count($matches) > 0) {
+                            // Remove unnessary element
+                            array_shift($matches);
+                            $paramNames = array_pop($paramNames);
 
-                    if (preg_match("#^{$routeRegex}$#", $uri, $matches)) {
-                        $callback = $cb;
-                        foreach ($paramNames[1] as $i => $name) {
-                            if (isset($matches[$i + 1])) {
-                                $request->setParam($name, $matches[$i + 1]);
+                            $callback = $cb;
+                            foreach ($paramNames as $index => $name) {
+                                $request->setParam($name, $matches[$index] ?? null);
                             }
+
+                            break;
                         }
-                        break;
                     }
                 }
             }
-        }
 
-        call_user_func($callback, $request, $response, ...array_values($request->getParams()));
+            // Reflect the callback to inject parameters
+            if (is_callable($callback)) {
+                $handler = new \ReflectionFunction($callback);
+            } else if (count($callback) === 2) {
+                $handler = new \ReflectionMethod($callback[0], $callback[1]);
+            }
+
+            $invokeArgs = [];
+            foreach ($handler->getParameters() as $rparam) {
+                $paramName = $rparam->getName();
+
+                // Handle Request and Response injection
+                if ($rparam->getType() instanceof \ReflectionNamedType) {
+                    $typeName = $rparam->getType()->getName();
+                    if ($typeName === Request::class) {
+                        $invokeArgs[$paramName] = $request;
+                        continue;
+                    }
+                    if ($typeName === Response::class) {
+                        $invokeArgs[$paramName] = $response;
+                        continue;
+                    }
+
+                    // Validate non-builtin types early
+                    // TODO: Maybe we should walk reading autoloader_classmap to create an DI container and inject them later?
+                    if (! $rparam->getType()->isBuiltin()) {
+                        throw new \InvalidArgumentException("Parameter {$paramName} must be a scalar type");
+                    }
+                }
+
+                // First check route parameters and query/post parameters
+                $paramValue = $request->getParam($paramName) ?? $request->get($paramName);
+
+                // If no value found, handle defaults
+                if ($paramValue === null) {
+                    if ($rparam->isDefaultValueAvailable()) {
+                        // Use parameter's default value from method signature
+                        $paramValue = $rparam->getDefaultValue();
+                    } else if (! $rparam->isOptional()) {
+                        // Required parameter not provided
+                        throw new \InvalidArgumentException("Parameter {$paramName} is required");
+                    } else {
+                        // Optional parameter without default - use type default
+                        $paramValue = match ($rparam->getType()->getName()) {
+                            'int'    => 0,
+                            'float', 'double' => 0.0,
+                            'string' => '',
+                            'bool'   => false,
+                            'array'  => [],
+                            default  => null,
+                        };
+                    }
+                }
+
+                $invokeArgs[$paramName] = $paramValue;
+            }
+
+            // If the handler is a non-static method, instantiate the class
+            // and get the closure from the instance
+            if ($handler instanceof \ReflectionMethod  && $handler->isStatic() === false) {
+                // TODO: Maybe support constructor injection in the future, by using a DI container
+                $instance = new ($handler->getDeclaringClass()->getName());
+                $handler  = $handler->getClosure($instance);
+            } else {
+                $handler = $handler->getClosure();
+            }
+
+            // Finally, invoke the handler with the prepared arguments
+            $handler(...$invokeArgs);
+        } catch (\Throwable $th) {
+            // TODO: need to dispatch an event?
+            // Events::dispatch('App.Error', $th);
+            if (isset(self::$error) && is_callable(self::$error)) {
+                $errorHandler = self::$error;
+                $errorHandler($request, $response, $th);
+            } else {
+                // Default error handling
+                $response->status(500)->send("Internal Server Error");
+            }
+        }
     }
 }
